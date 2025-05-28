c76168a8234639a862f8104fdb8052084db4dcaf
Szerl0k <mikojoj@gmail.com>
2025-05-28 11:49:26 +0200

    APBD_CW5 init

---
+++ APBD_CW5/Program.cs
@@ -0,0 +1,65 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace APBD_CW5;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+        builder.Services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "APBD_CW5 API",
+                Version = "v1",
+                Description = "REST API with EF for prescription management system",
+                Contact = new OpenApiContact
+                {
+                    Name = "API Support",
+                    Email = "support@example.com",
+                    Url = new Uri("https://www.example.com/support")
+                },
+                License = new OpenApiLicense()
+                {
+                    Name = "MIT License",
+                    Url = new Uri("https://opensource.org/licenses/MIT")
+                }
+            });
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "APBD_CW5 API v1");
+                
+                // Basic UI Customization
+                c.DocExpansion(DocExpansion.List);
+                c.DefaultModelsExpandDepth(0);
+                c.DisplayRequestDuration();
+                c.EnableFilter();
+            });
+        }
+        
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+        
+        app.MapControllers();
+        
+        app.Run();
+    }
+}
